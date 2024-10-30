d9daa79788c5426792963e36128703c1b40a0569
Carlos Alberto Salcedo Rodriguez <csalcedo@utb.edu.co>
2024-10-29 21:42:32 -0500

    se remplaza cache storage por API con conection a bases de datos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import axios from "axios"; 
 
 function App() {
-  const [notes, newNote] = React.useState([]);
-  function Clicked(nota) {
-    newNote((prev) => {
-      console.log(nota);
-      return [...prev, nota];
-    });
-  }
-  function Deleted(nota) {
-    newNote((prev) => {
-      return prev.filter((item, index) => {
-        console.log(!index);
-        return index !== nota;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/noteapp");
+      setNotes(response.data);
+    } catch (error) {
+      console.error("Error fetching notes:", error);
+    }
+  };
+
+  const addNote = async (newNote) => {
+    try {
+      console.log(newNote)
+      await axios.post("http://localhost:3000/noteapp", newNote);
+      setNotes((prevNotes) => [...prevNotes, newNote]); 
+    } catch (error) {
+      console.error("Error adding note:", error);
+    }
+  };
+  const deleteNote = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/noteapp/${id}`);
+      console.log("Eliminando nota con id:", id);
+      setNotes((prevNotes) => {
+        console.log("Notas previas:", prevNotes);
+        return prevNotes.filter((note) => note.id !== id);
       });
-    });
-  }
+    } catch (error) {
+      console.error("Error deleting note:", error);
+    }
+  };
 
   return (
     <div>
       <Header />
-      <CreateArea onAdd={Clicked} />
-      {notes.map((item, index) => {
-        return (
-          <Note
-            key={index}
-            id={index}
-            title={item.title}
-            content={item.content}
-            onDelete={Deleted}
-          />
-        );
-      })}
+      <CreateArea onAdd={addNote} />
+      {notes.map((item) => (
+        <Note
+          key={item.id} 
+          id={item.id} 
+          title={item.title}
+          content={item.content}
+          onDelete={() => deleteNote(item.id)} 
+        />
+      ))}
       <Footer />
     </div>
   );
